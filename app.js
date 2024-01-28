9f2860d9ff2c2dcd8c2683f2215f91dd9ea6721b
Ayush Mahajan <mahajan.ayush2306@gmail.com>
2024-01-28 13:29:51 +0530

    Update to hello anshika

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = parseInt(process.env.port) || 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World v2!')
+  res.send('Hello Anshika!')
 })
 
 app.get('/kill', (req, res) => {
